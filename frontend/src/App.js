b99d5ae4988a2ceddbb36d98bb52dc29f6b0d994
ArijesaMuja <arijesa.muja@student.uni-pr.edu>
2024-03-31 15:52:29 +0200

    Frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './component/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ color: 'blue', textAlign: 'center' }}>Order your food</h1>
+    </div>
+  );
+}
+
+export default App;
