d0b50715beb61e4784f01891852bd6b6e80f2462
Rinesa Bislimi <118773246+RinesaBislimi@users.noreply.github.com>
2024-04-07 14:12:42 +0200

    Implement carousel section for top meals on homepage

---
+++ frontend/src/component/Home/topMeel.js
@@ -0,0 +1,33 @@
+export const topMeels = [
+    {
+        image:"images/topMeelsPizza.jpg",
+        title:"Pizza"
+    },
+    {
+        image:"images/topMeelsBurger.jpg",
+        title:"Burger"
+    },
+    {
+        image:"images/topMeelsSandwich.jpg",
+        title:"Sandwich"
+    },
+    {
+        image:"images/topMeelsBreakfast.jpg",
+        title:"Breakfast"
+    },
+    {
+        image:"images/topMeelsCake.jpg",
+        title:"Cake"
+    },
+    {
+        image:"images/topMeelsRice.jpg",
+        title:"Fried Rice"
+    },
+    {
+        image:"images/topMeelsSalat.jpg",
+        title:"Salats"
+    },
+
+
+
+]
