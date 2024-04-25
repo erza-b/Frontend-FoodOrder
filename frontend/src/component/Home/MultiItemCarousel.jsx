f21faba08afd26b2d70c23e75b68f1a953cbe63b
Erza Bërbatovci <116487812+erza-b@users.noreply.github.com>
2024-04-25 23:45:05 +0200

    Add autoplay settings and remove arrows in slider component

--- frontend/src/component/Home/MultiItemCarousel.jsx
+++ frontend/src/component/Home/MultiItemCarousel.jsx
@@ -7,9 +7,19 @@ import CarouselItem from './CarouselItem';
 
 
 export const MultiItemCarousel = () => {
+    const settings={
+        dots:true,
+        infinite:true,
+        speed:500,
+        slidesToShow:5,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed:2000,
+        arrows:false
+    };
     return (
         <div>
-            <Slider>
+            <Slider {...settings}>
                 {topMeels.map((item)=> <CarouselItem image={item.image} title={item.title}/>)}
             </Slider>
         </div>
