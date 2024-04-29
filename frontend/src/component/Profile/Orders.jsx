a92d7e7f637393f8586e206699a419b7fb81b836
Erza Bërbatovci <116487812+erza-b@users.noreply.github.com>
2024-04-29 16:39:15 +0200

    Create Orders component with title and container

--- frontend/src/component/Profile/Orders.jsx
+++ frontend/src/component/Profile/Orders.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Orders = () => {
   return (
-    <div>
+    <div className='flex items-center flex-col'>
+      <h1 className='text-xl text-center py-7 font-semibold'>My orders</h1>
+      <div className='space-y-5 w-full lg:w-1/2'>
+        {
+
+        }
+      </div>
       
     </div>
   );
