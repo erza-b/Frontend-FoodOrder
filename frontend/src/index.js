489e62032b87cdb9aee3748db9b8416b0127f189
Rinesa Bislimi <118773246+RinesaBislimi@users.noreply.github.com>
2024-05-01 21:17:24 +0200

    Fixed Issues in Registration Form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
+import { store } from './component/State/store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
