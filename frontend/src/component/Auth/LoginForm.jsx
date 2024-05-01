f70cf8d4ca1aab2fb33b6881774a5fa94ec797ca
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2024-05-01 20:46:49 +0200

    Added Redux Actions for User Authentication and Favorites

--- frontend/src/component/Auth/LoginForm.jsx
+++ frontend/src/component/Auth/LoginForm.jsx
@@ -1,49 +1,56 @@
-import { Button, TextField, Typography } from '@mui/material'
-import { Field, Form, Formik } from 'formik'
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+// LoginForm.jsx
+import React from 'react';
+import { Button, TextField, Typography } from '@mui/material';
+import { Field, Form, Formik } from 'formik';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { loginUser } from '../State/Authentication/Action';
 
-const initialValues={
-    email:"",
-    password:""
-}
+const initialValues = {
+    email: "",
+    password: ""
+};
+
 function LoginForm() {
-    const navigate=useNavigate()
-    const handleSubmit=()=>{
-        
-    }
-  return (
-    <div>
-        <Typography variant='h5' className='text-center'>
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleSubmit = (values) => {
+        dispatch(loginUser({ userData: values, navigate }));
+    };
+
+    return (
+        <div>
+            <Typography variant='h5' className='text-center'>
                 Login
-        </Typography>
-        <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-         <Form>
-         <Field
-                as={TextField}
-                name="email"
-                label="email"
-                fullWidth
-                variant="outlined"  
-                margin='normal'                      
-                />
-            <Field
-                as={TextField}
-                name="password"
-                label="password"
-                fullWidth
-                variant="outlined"  
-                margin='normal'                      
-                />
-               <Button sx={{mt:2, padding:'1rem'}} fullWidth type='submit'variant='contained' >Login</Button>
-         </Form>
-        </Formik>
-        <Typography variant='body2' align='center' sx={{mt:3}}>
-            Don't have an account?
-            <Button size='small' onClick={()=>navigate("/account/register")}>register</Button>
-        </Typography>
-    </div>
-  )
+            </Typography>
+            <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+                <Form>
+                    <Field
+                        as={TextField}
+                        name="email"
+                        label="email"
+                        fullWidth
+                        variant="outlined"
+                        margin='normal'
+                    />
+                    <Field
+                        as={TextField}
+                        name="password"
+                        label="password"
+                        fullWidth
+                        variant="outlined"
+                        margin='normal'
+                    />
+                    <Button sx={{ mt: 2, padding: '1rem' }} fullWidth type='submit' variant='contained'>Login</Button>
+                </Form>
+            </Formik>
+            <Typography variant='body2' align='center' sx={{ mt: 3 }}>
+                Don't have an account?
+                <Button size='small' onClick={() => navigate("/account/register")}>register</Button>
+            </Typography>
+        </div>
+    );
 }
 
-export default LoginForm
+export default LoginForm;
