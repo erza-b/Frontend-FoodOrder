95803c644f4c353429a1b788c66e421749b3674d
ArijesaMuja <arijesa.muja@student.uni-pr.edu>
2024-05-12 21:47:25 +0200

    Order and RestaurantOrder

--- frontend/src/component/State/store.js
+++ frontend/src/component/State/store.js
@@ -3,12 +3,16 @@ import { authReducer } from "./Authentication/Reducer";
 import{thunk} from "redux-thunk";
 import restaurantReducer from "./Restaurant/Reducer";
 import menuItemReducer from "./Menu/Reducer";
+import cartReducer from "./Cart/Reducer";
+import { orderReducer } from "./Order/Reducer";
 
 const rooteReducer=combineReducers({
     auth:authReducer,
     restaurant:restaurantReducer,
-    menu:menuItemReducer
-})
+    menu:menuItemReducer,
+    cart:cartReducer,
+    order:orderReducer
+});
 
 export const store=legacy_createStore(rooteReducer,applyMiddleware(thunk))
 
