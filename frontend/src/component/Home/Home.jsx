8c023a1a1f63bec22434116fb09c5bb14b1de3e5
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2024-05-13 00:58:42 +0200

    Implemented Redux actions and a reducer to manage ingredient and ingredient category state in the admin interface.

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Home.css"
 import MultiItemCarousel from './MultiItemCarousel'
 import RestaurantCard from '../Restaurant/RestaurantCard'
 import { Auth } from '../Auth/Auth'
+import { useDispatch } from 'react-redux'
+import {getAllRestaurantsAction} from'../State/Restaurant/Action'
 
 const restaurant=[1,1,1,1,1,1,1,1]
 
 const Home = () => {
+  const dispatch=useDispatch()
+  const jwt=localStorage.getItem("jwt")
+  useEffect(()=>{
+dispatch(getAllRestaurantsAction(jwt))
+  },[])
+
+
   return (
     <div className='pb-10'>
         <section className='banner -z-50 relative flex flex-col justify-center
