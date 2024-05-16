eb56f90b2ddcb8411011f476a4571f34abb72e0c
Erza Bërbatovci <116487812+erza-b@users.noreply.github.com>
2024-05-16 12:10:43 +0200

    Add photo upload feature to CreateRestaurantForm component

---
+++ frontend/src/AdminComponent/CreateRestaurantForm/CreateRestaurantForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { CircularProgress, Grid } from '@mui/material';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate'; // Import AddPhotoAlternateIcon
+import { useFormik } from 'formik';
+
+const CreateRestaurantForm = () => {
+    const [uploadImage, setUploadImage] = useState(false);
+    const formik = useFormik();
+
+    const handleImageChange = (e) => {
+        // Handle image change here
+    };
+
+    return (
+        <div className='py-10 lg:flex items-center justify-center min-h-screen'>
+            <h1 className='font-bold text-2xl text-center py-2'>
+                Add new Restaurant
+            </h1>
+            <form onSubmit={formik.handleSubmit} className='space-y-4'>
+                <Grid container spacing={2}>
+                    <Grid className='flex flex-wrap gap-5' item xs={12}>
+
+                        <input
+                            accept='image/*'
+                            id='fileInput'
+                            style={{ display: "none" }}
+                            onChange={handleImageChange}
+                            type="file" />
+
+                        <label className='relative' htmlFor='fileInput'>
+                            <span className='w-24 h-24 cursor-pointer flex items-center justify-center p-3 border rounded-md border-gray-600'>
+                                <AddPhotoAlternateIcon className='text-white' /> {/* Use imported AddPhotoAlternateIcon */}
+                            </span>
+                            {
+                                uploadImage && <div className='absolute left-0 right-0 top-0 bottom-0 w-24 h-24 flex justify-center items-center'>
+                                    <CircularProgress/>
+                                </div>
+                            }
+                        </label>
+
+                        <div className='flex flex-wrap gap-2'>
+                            {[1,1,1].map((image,index)=><div key={index}>
+                                <img className='w-24 h-24 object-cover'
+                                    src="public\images\burger-with-melted-cheese.jpg" alt=""/>
+                                </div>)}
+                        </div>
+
+                    </Grid>
+                </Grid>
+            </form>
+        </div>
+    );
+}
+
+export default CreateRestaurantForm;
