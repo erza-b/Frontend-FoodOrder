6236753c5c90e744acce3193d2d749ddccc62755
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2024-05-17 11:22:16 +0200

    Add modal for creating new event in Events component

--- frontend/src/AdminComponent/Events/Events.jsx
+++ frontend/src/AdminComponent/Events/Events.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Button, Modal, Box } from '@mui/material';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export const Events = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <div>Events</div>
+    <div>
+      <div className='p-5'>
+        <Button variant='contained'>Create Ne Event </Button>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          
+        </Box>
+      </Modal>
+
+      </div>
+      </div>
   )
 }
