ceb8ae6be750c8902635e990968c128647d14a94
Vijola Neziri <vijola.neziri@student.uni-pr.edu>
2024-05-17 13:24:27 +0200

    Implemented event creation form using MUI components and Day.js for date handling

--- frontend/src/AdminComponent/Events/Events.jsx
+++ frontend/src/AdminComponent/Events/Events.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Button, Modal, Box } from '@mui/material';
+import { Button, Modal, Box, TextField, Grid } from '@mui/material';
+import { LocalizationProvider, DateTimePicker } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
 
 const style = {
   position: 'absolute',
@@ -17,23 +20,103 @@ export const Events = () => {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [formValues, setFormValues] = React.useState({
+    image: "",
+    location: "",
+    name: "",
+    startedAt: null,
+    endsAt: null
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted:', formValues);
+    handleClose(); // Close the modal after submission
+  };
+
+  const handleFormChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  const handleDateChange = (date, dateType) => {
+    setFormValues({ ...formValues, [dateType]: date });
+  };
 
   return (
     <div>
       <div className='p-5'>
-        <Button variant='contained'>Create Ne Event </Button>
+        <Button onClick={handleOpen} variant='contained'>Create New Event</Button>
         <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          
-        </Box>
-      </Modal>
-
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <form onSubmit={handleSubmit}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <TextField
+                    name="image"
+                    label="Image URL"
+                    variant="outlined"
+                    fullWidth
+                    value={formValues.image}
+                    onChange={handleFormChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="location"
+                    label="Location"
+                    variant="outlined"
+                    fullWidth
+                    value={formValues.location}
+                    onChange={handleFormChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    name="name"
+                    label="Event Name"
+                    variant="outlined"
+                    fullWidth
+                    value={formValues.name}
+                    onChange={handleFormChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateTimePicker
+                      renderInput={(props) => <TextField {...props} />}
+                      label="Start Date and Time"
+                      value={formValues.startedAt}
+                      onChange={(newValue) => handleDateChange(newValue, "startedAt")}
+                      inputFormat="MM/DD/YYYY hh:mm A"
+                      fullWidth
+                    />
+                  </LocalizationProvider>
+                </Grid>
+                <Grid item xs={12}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateTimePicker
+                      renderInput={(props) => <TextField {...props} />}
+                      label="End Date and Time"
+                      value={formValues.endsAt}
+                      onChange={(newValue) => handleDateChange(newValue, "endsAt")}
+                      inputFormat="MM/DD/YYYY hh:mm A"
+                      fullWidth
+                    />
+                  </LocalizationProvider>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button type="submit" variant="contained" fullWidth>Submit</Button>
+                </Grid>
+              </Grid>
+            </form>
+          </Box>
+        </Modal>
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
